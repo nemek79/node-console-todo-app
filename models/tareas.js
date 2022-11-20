9bbe1c50fbb2ab072e6745b75078d160d44dbc79
David Alonso <davidalonso79@gmail.com>
2022-11-20 19:28:54 +0100

    desarrollo

---
+++ models/tareas.js
@@ -0,0 +1,17 @@
+/**
+ *  _listado: 
+ *      {'uuid-121212121212: {id: ..., desc: asddddd, completadoEn: xxxx}}
+ */
+
+class Tareas {
+
+    _listado = {};
+
+    constructor() {
+        this._listado = {};
+    }
+
+
+}
+
+module.exports = Tareas;
